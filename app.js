84b5780cdc8dd600a4b24a00881b5fc7abfeb719
ARSHamirali <arshamirali604@gmail.com>
2024-09-20 12:16:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// Testing set of Condition chapter(13)
+
+// logical condition
+
+// var gender =prompt("Enter your gender")
+// var Weight =+prompt("Enter your weight")
+// var time =+prompt("Enter time")
+
+// if (gender == "male"||"female" && Weight > 120 && time > 6) {
+//     console.log("You are eligible for gym membership")
+// }
+// else{
+//     console.log("You are not eligible for gym membership")
+// }
+
+
+// var Prolang = prompt("Enter your programming language")
+// if (Prolang === "JS") {
+//     var city = prompt("Enter your city")
+//     if (city === "Karachi") {
+//         var exp = +prompt("Enter your experience")
+//         if (exp >= 2) {
+//             console.log("You are eligible for this job")
+
+//         }
+//         else {
+//             console.log("Experience lao")
+//         }
+//     }
+//     else {
+//         console.log(" karachi ajao")
+//     }
+// }
+// else {
+//     console.log("JS seekho..")
+// }
+
+
+// ARRAYS (CHAPTER#15)
+// var Information = [5, "Arsh", 2,undefined,50,"Amir",123,"Ali",true]
+
+//  console.log(Information.length )
+
+//  console.log(Information[1])
+//  console.log(Information[2])
+//  console.log(Information[3])
+//  console.log(Information[4])
+//  console.log(Information[5])
+//  console.log(Information[6])
+//  console.log(Information[7])
+//  console.log(Information[8])
+//  console.log(Information[9]) undefined "NO" value store.
+
